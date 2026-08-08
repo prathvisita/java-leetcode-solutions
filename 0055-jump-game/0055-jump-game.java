649430976af26285b5532c8d823fe61dfd8ccf1d
prathvisita <prathvisita67@gmail.com>
2026-08-08 15:37:20 +0530

    Time: 2 ms (88.78%), Space: 47.8 MB (81.74%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       // int i=0;
+        int maxjump=0;
+        for(int i=0;i<nums.length;i++){
+           if(i>maxjump){
+            return false;
+           }
+           maxjump=Math.max(maxjump,i+nums[i]);
+           if(maxjump>=nums.length-1){
+            return true;
+           }
+        }
+        return true;
+    }
+}
