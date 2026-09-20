e144d04bdea799a23b1da2b4ba1224e68e2e97da
prathvisita <prathvisita67@gmail.com>
2026-09-20 10:35:54 +0530

    Time: 1686 ms (5.01%), Space: 48.4 MB (6.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,23 +3,23 @@ class Solution {
         if(s.length()==0){
             return 0;
         }
-     int i=0;
-     int j=1;
-     int length=1;
-     int Maxlength=1;
-     while(i<j&&j<s.length()){
-        if(s.substring(i,j).indexOf(s.charAt(j))==-1){
-             length++;
-            if(length>Maxlength){
-              Maxlength=length;
+        int i=0;
+        int j=1;
+        int length=1;
+        int maxlength=1;
+        while(i<j&&j<s.length()){
+            if(s.charAt(i)!=s.charAt(j)&&s.substring(i,j).indexOf(s.charAt(j))==-1){
+                length++;
+                if(length>maxlength){
+                    maxlength=length;
+                }
+                j++;
+            }else{
+                length=1;
+                i++;
+                j=i+1;
+            }
         }
-            j++;
-        }else{
-            length=1;
-            i++;
-            j=i+1;
-        }
-     }
-      return Maxlength;
+        return maxlength;
     }
 }
